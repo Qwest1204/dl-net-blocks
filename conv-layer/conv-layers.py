02793b243a7844d5e73cb9d773876449dee10b5a
Qwest1204 <myPCwithUbuntu@gmail.com>
2026-05-20 22:03:19 +0300

    add ResBlock BottleResBlock InceptionModule

--- conv-layer/conv-layers.py
+++ conv-layer/conv-layers.py
@@ -2,15 +2,130 @@ import torch
 import torch.nn as nn
 
 class ResidualBlock(nn.Module):
-    ...
+    def __init__(self, in_channels, out_channels, stride=1, downsample=None):
+        super(ResidualBlock, self).__init__()
+        
+        self.conv1 = nn.Conv2d(in_channels, out_channels, kernel_size=3, stride=stride,
+                               padding=1, bias=False)
+        self.bn1 = nn.BatchNorm2d(out_channels)
+        
+        self.conv2 = nn.Conv2d(out_channels, out_channels, kernel_size=3, stride=1,
+                               padding=1, bias=False)
+        self.bn2 = nn.BatchNorm2d(out_channels)
+
+        self.downsample = downsample
+        
+        self.relu = nn.ReLU(inplace=True)
     
+    def forward(self, x):
+        identity = x
+        
+        out = self.conv1(x)
+        out = self.bn1(out)
+        out = self.relu(out)    
+        
+        out = self.conv2(out)
+        out = self.bn2(out)
+        
+        if self.downsample is not None:
+            identity = self.downsample(x)
+        
+        out += identity
+        out = self.relu(out)
+        
+        return out
     
+ 
 class BottleneckResidualBlock(nn.Module):
-    ...
+    expansion = 4
+    
+    def __init__(self, in_channels, internal_channels, stride=1, downsample=None):
+        super(BottleneckResidualBlock, self).__init__()
+        
+        out_channels = internal_channels*self.expansion
+        
+        self.conv1 = nn.Conv2d(in_channels, internal_channels, kernel_size=1,
+                               bias=False)
+        self.bn1   = nn.BatchNorm2d(internal_channels)
+
+        self.conv2 = nn.Conv2d(internal_channels, internal_channels, kernel_size=3,
+                               stride=stride, padding=1, bias=False)
+        self.bn2   = nn.BatchNorm2d(internal_channels)
+
+        self.conv3 = nn.Conv2d(internal_channels, out_channels, kernel_size=1,
+                               bias=False)
+        self.bn3   = nn.BatchNorm2d(out_channels)
+
+        self.downsample = downsample
+        self.relu = nn.ReLU(inplace=True)
+    
+    def forward(self, x):
+        identity = x
+
+        out = self.conv1(x)
+        out = self.bn1(out)
+        out = self.relu(out)
+
+        out = self.conv2(out)
+        out = self.bn2(out)
+        out = self.relu(out)
+
+        out = self.conv3(out)
+        out = self.bn3(out)
+
+        if self.downsample is not None:
+            identity = self.downsample(x)
+
+        out += identity
+        out = self.relu(out)
+
+        return out
 
 
 class InceptionModule(nn.Module):
-    ...
+    def __init__(self, in_channels, ch1x1, ch3x3_reduce, ch3x3, ch5x5_reduce, ch5x5, pool_proj):
+        super(InceptionModule, self).__init__()
+        
+        self.branch1 = nn.Sequential(
+            nn.Conv2d(in_channels, ch1x1, kernel_size=1),
+            nn.BatchNorm2d(ch1x1),
+            nn.ReLU(inplace=True)
+        )
+        
+        self.branch2 = nn.Sequential(
+            nn.Conv2d(in_channels, ch3x3_reduce, kernel_size=1),
+            nn.BatchNorm2d(ch3x3_reduce),
+            nn.ReLU(inplace=True),
+            nn.Conv2d(ch3x3_reduce, ch3x3, kernel_size=3, padding=1),
+            nn.BatchNorm2d(ch3x3),
+            nn.ReLU(inplace=True)
+        )
+                
+        self.branch3 = nn.Sequential(
+            nn.Conv2d(in_channels, ch5x5_reduce, kernel_size=1),
+            nn.BatchNorm2d(ch5x5_reduce),
+            nn.ReLU(inplace=True),
+            nn.Conv2d(ch5x5_reduce, ch5x5, kernel_size=5, padding=2),
+            nn.BatchNorm2d(ch5x5),
+            nn.ReLU(inplace=True)
+        )
+                        
+        self.branch4 = nn.Sequential(
+            nn.MaxPool2d(kernel_size=3, stride=1, padding=1),
+            nn.Conv2d(in_channels, pool_proj, kernel_size=1),
+            nn.BatchNorm2d(pool_proj),
+            nn.ReLU(inplace=True),
+        )
+    
+    def forward(self, x):
+        
+        out1 = self.branch1(x)
+        out2 = self.branch2(x) 
+        out3 = self.branch3(x) 
+        out4 = self.branch4(x) 
+        
+        out = torch.cat([out1, out2, out3, out4], dim=1)
+        return out
     
     
 class DenseBlock(nn.Module):
