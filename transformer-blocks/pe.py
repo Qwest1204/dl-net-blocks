d759d3f9c98b90b6d7b5a3d6d41291aa4d7b5674
Qwest1204 <myPCwithUbuntu@gmail.com>
2026-05-20 21:37:16 +0300

    add pe

---
+++ transformer-blocks/pe.py
@@ -0,0 +1,17 @@
+import torch
+import torch.nn as nn
+
+class SinusoidalPositionalEncoding(nn.Module):
+    ...
+    
+
+class LearnableAbsolutePositionalEmbeddings(nn.Module):
+    ...
+
+
+class RotaryPositionEmbedding(nn.Module):
+    ...
+    
+
+class LearnedRelativePositionalEmbedding(nn.Module):
+    ...
